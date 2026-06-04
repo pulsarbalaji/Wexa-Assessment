bdd6668dae39b85c7e4aef7a79f17728a3d0faaa
pulsarbalaji <vijayk070898@gmail.com>
2026-06-05 00:51:00 +0530

    feat: import dj_database_url for database configuration

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 from celery.schedules import (crontab)
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
